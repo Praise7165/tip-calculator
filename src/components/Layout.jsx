b9bd1ab9f7d81f6d8195a1dd7976cc08e9fa8ec1
Omolade Lekan <omoladelekan99@gmail.com>
2025-10-21 07:11:00 +0100

    and more changes

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -3,7 +3,14 @@ export default function Layout({ children }) {
     <div className="section">
       <div className="container flex">
         <div style={{ width: "100%", justifyItems: "center" }}>
-          <h1 style={{ fontSize: "32px", marginBottom: "1.5rem" }}>
+          <h1
+            style={{
+              marginInline: "auto",
+              textAlign: "center",
+              fontSize: "32px",
+              marginBottom: "1.5rem",
+            }}
+          >
             Bill Splitter
           </h1>
           <div className="calculator flex rounded-lg">{children}</div>
