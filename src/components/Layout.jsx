d17eff38644cae6692e3ca56b6d978700079b611
Omolade Lekan <omoladelekan99@gmail.com>
2025-10-21 07:02:48 +0100

    fixed responsiveness and overflow issue on mobile

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -2,7 +2,12 @@ export default function Layout({ children }) {
   return (
     <div className="section">
       <div className="container flex">
-        <div className="calculator flex rounded-lg">{children}</div>
+        <div style={{ width: "100%", justifyItems: "center" }}>
+          <h1 style={{ fontSize: "32px", marginBottom: "1.5rem" }}>
+            Bill Splitter
+          </h1>
+          <div className="calculator flex rounded-lg">{children}</div>
+        </div>
       </div>
     </div>
   );
